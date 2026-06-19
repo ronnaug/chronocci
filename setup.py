577950214fcd8dcf298b2adb2e03f5bd47fe47b7
ronnaug <inrainbow@mail.ru>
2026-06-19 12:48:18 +0300

    Fix fate probabilities check_sum_tol and bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chronocci",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="your.email@example.com",
     description="Chronological Inference of Multi-Lineage Cell-Cell Interactions Using CellRank and LIANA",
